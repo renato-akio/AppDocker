d45086806ef150d2e97af366b956aea392a5fcf7
Renato Hossotani <renato.hossotani@indigosoft.com.br>
2019-10-16 22:35:12 -0300

    first commit

---
+++ AppDocker/Migrations/20190218181552_EFCoreCodeFirstSample.Models.EmployeeContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppDocker.Migrations
+{
+    public partial class AppDockerModelsEmployeeContextSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1L, new DateTime(1980, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "teste@teste.com", "Primeiro", "Teste", "999-888-7777" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 2L, new DateTime(1981, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "teste2@teste2.com", "Segundo", "Teste", "111-222-3333" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2L);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# AppDocker" 
+# .NET Core Web API with EF Core Code-First approach
+## https://code-maze.com/net-core-web-api-ef-core-code-first
